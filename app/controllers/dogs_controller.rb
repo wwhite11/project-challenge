12b5a299e585be034a6b323b5389fe21ced402b8
wwhite11 <2winstonrwhite@gmail.com>
2021-02-07 02:33:27 -0500

    Added Pagination, Associated dogs with owners, editing only by owner, Displayed the ad.jpg as well

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,11 @@
 class DogsController < ApplicationController
-  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  before_action :set_dog, only: [:show, :edit,  :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :create, :update]
 
   # GET /dogs
   # GET /dogs.json
   def index
-    @dogs = Dog.all
+    @dogs = Dog.all.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /dogs/1
@@ -19,18 +20,25 @@ class DogsController < ApplicationController
 
   # GET /dogs/1/edit
   def edit
+    if current_user.id != @dog.user_id
+      redirect_to root_path, :alert => "Not your cute dog"
+    end
   end
 
   # POST /dogs
   # POST /dogs.json
   def create
     @dog = Dog.new(dog_params)
-
+    current_user.dogs << @dog
     respond_to do |format|
       if @dog.save
-        @dog.images.attach(params[:dog][:image]) if params[:dog][:image].present?
+        if params[:dog][:images].present?
+          params[:dog][:images].each do |image|
+            @dog.images.attach(image)
+          end
+        end
 
-        format.html { redirect_to @dog, notice: 'Dog was successfully created.' }
+        format.html { redirect_to @dog, notice: 'The cute Dog was successfully created.' }
         format.json { render :show, status: :created, location: @dog }
       else
         format.html { render :new }
@@ -46,7 +54,7 @@ class DogsController < ApplicationController
       if @dog.update(dog_params)
         @dog.images.attach(params[:dog][:image]) if params[:dog][:image].present?
 
-        format.html { redirect_to @dog, notice: 'Dog was successfully updated.' }
+        format.html { redirect_to @dog, notice: 'The cute Dog was successfully updated.' }
         format.json { render :show, status: :ok, location: @dog }
       else
         format.html { render :edit }
@@ -60,7 +68,7 @@ class DogsController < ApplicationController
   def destroy
     @dog.destroy
     respond_to do |format|
-      format.html { redirect_to dogs_url, notice: 'Dog was successfully destroyed.' }
+      format.html { redirect_to dogs_url, notice: 'The cute Dog was successfully removed.' }
       format.json { head :no_content }
     end
   end
@@ -73,6 +81,6 @@ class DogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dog_params
-      params.require(:dog).permit(:name, :description, :images)
+      params.require(:dog).permit(:name, :description, images: [])
     end
 end
